bb90c81279856802bcb95150fb51bcf6f4739bbc
pj-mathematician <55710610+pj-mathematician@users.noreply.github.com>
2020-08-18 08:50:44 +0530

    Create reference.java
    Initialization of string objects

---
+++ reference.java
@@ -0,0 +1,8 @@
+public class reference {
+	public static void main(String[] args) {
+		String s1 = "Hello World!"; //not so general, exclusively for strings
+		String s2 = new  String("Hello World! 2"); //general works everywhere
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
